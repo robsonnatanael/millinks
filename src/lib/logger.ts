68b444e8994e4306a36e746986a48cf9e27107a5
Robson Natanael <natanaelrobson@gmail.com>
2026-03-15 11:32:16 -0300

    refactor(logger): improve type safety by replacing any with unknown

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,19 +1,17 @@
-// TODO: adjust types
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export const logger = {
-  info: (msg: string, data?: any) => {
+  info: (msg: string, data?: unknown) => {
     console.log(
       `[INFO] [${new Date().toLocaleString('pt-BR')}] ${msg}`,
       data || ''
     );
   },
-  warn: (msg: string, data?: any) => {
+  warn: (msg: string, data?: unknown) => {
     console.warn(
       `[WARN] [${new Date().toLocaleString('pt-BR')}] ${msg}`,
       data || ''
     );
   },
-  error: (msg: string, error?: any) => {
+  error: (msg: string, error?: unknown) => {
     console.error(
       `[ERROR] [${new Date().toLocaleString('pt-BR')}] ${msg}`,
       error || ''
